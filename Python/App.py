bd5d1ce39e97ff4c18de330fc7c28cd59da105ec
JhonnyNicolas85 <87675875+JhonnyNicolas85@users.noreply.github.com>
2022-04-17 22:34:14 -0500

    Agregando los elementos de la tarea 1 de python

---
+++ Python/App.py
@@ -0,0 +1,10 @@
+
+#Hola, soy [tu_nombre]
+nombre = "Jhonny Martínez"
+print("Hola, soy " + nombre)
+
+#Estoy empezando el curso de python
+print("Estoy empezando el curso de python")
+
+#Estoy empezando el curso de python
+print("Espero aprender mucho")
