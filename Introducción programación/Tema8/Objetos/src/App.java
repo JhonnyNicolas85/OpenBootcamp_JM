47107ed6ac2851ed24c2a5aa224881e951a94b2a
JhonnyNicolas85 <87675875+JhonnyNicolas85@users.noreply.github.com>
2022-04-19 12:53:01 -0500

    Agregando el ejercicio 8 - Introducción a la programación

---
+++ Introducción programación/Tema8/Objetos/src/App.java
@@ -0,0 +1,47 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        Persona persona1 = new Persona();
+        // Edad
+        persona1.setEdad(23);
+        System.out.println(persona1.getEdad());
+
+        // Nombre
+        persona1.setNombre("Jhonny Martínez");
+        System.out.println(persona1.getNombre());
+
+        //Teléfono
+        persona1.setTelefono(3022481);
+        System.out.println(persona1.getTelefono());
+
+    }
+}
+
+class Persona {
+    int edad;
+    String nombre;
+    int telefono;
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public int getEdad() {
+        return this.edad;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return this.nombre;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+
+    public int getTelefono() {
+        return this.telefono;
+    }
+}
